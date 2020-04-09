4ec3c2c0ec1286978d13038152a8fffc06212913
thewrenchess <ydxu2147483648@gmail.com>
2020-04-09 07:13:03 -0700

    able to show tweets

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import Layout from './Layout'
-import { TwitterTimelineEmbed } from 'react-twitter-embed'
+import {
+  TwitterTimelineEmbed,
+  TwitterTweetEmbed
+} from 'react-twitter-embed'
 import {
   get_request_token,
   is_signed_in,
@@ -12,10 +15,16 @@ import '../assets/bootstrap-social.css'
 const Home = () => {
   const feed_window_style = {
     height: `${window.innerHeight - 200}px`,
-    scrollY: 'auto'
+    overflow: 'scroll'
+  }
+  const cog_style = {
+    color: '#5CB8FF',
+    'fontSize': '100px'
   }
 
   const [user, set_user] = useState(is_signed_in())
+  const [tweets, set_tweets] = useState([])
+  const [filtered_tweets, set_filtered_tweets] = useState([])
   const [is_loading, set_is_loading] = useState(false)
 
   const siginin = (event) => {
@@ -37,12 +46,33 @@ const Home = () => {
   }
 
   useEffect(() => {
+    const sort_tweets = (tweets) => {
+      return tweets.sort((a, b) => {
+        const a_created_at = new Date(a.tweet_created_at)
+        const b_created_at = new Date(b.tweet_created_at)
+        return b_created_at - a_created_at
+      })
+    }
+
     if (user) {
+      set_is_loading(true)
       get_tweets(user.user_id)
-        .then(data => console.log(data))
+        .then(data => {
+          const {
+            tweet_array
+          } = data
+          const _tweets = sort_tweets(tweet_array)
+          set_tweets(_tweets)
+          console.log(_tweets)
+          set_is_loading(false)
+        })
         .catch(err => console.log(err))
     }
   }, [user])
+
+  useEffect(() => {
+    set_filtered_tweets(tweets)
+  }, [tweets])
 
   return (
     <Layout
@@ -59,7 +89,21 @@ const Home = () => {
               className='col-md-8'
               style={ feed_window_style }
             >
-              feed here
+              {
+                is_loading ? (
+                  <i
+                    className='fa fa-cog fa-spin'
+                    style={ cog_style }
+                  />
+                ) : (
+                  filtered_tweets.map(tweet => (
+                    <TwitterTweetEmbed
+                      key={ tweet.tweet_id }
+                      tweetId={ tweet.tweet_id }
+                    />
+                  ))
+                )
+              }
             </div>
             <div className='col-md-4'>
             <TwitterTimelineEmbed
